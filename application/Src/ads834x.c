674670a7e1014e2bd7b6011ef4ed66d3c79c2d1e
Enforcer83 <jkputz@gmail.com>
2025-07-01 08:26:29 -0400

    Update ads834x.c

--- application/Src/ads834x.c
+++ application/Src/ads834x.c
@@ -28,7 +28,7 @@
 
 #include "ads834x.h"
 
-static uint8_t tmp_buf[3];
+static uint8_t tmp_buf[3] = {0};
 
 uint16_t ADS834x_GetData(sensor_t * sensor, uint8_t channel)
 {
@@ -40,13 +40,33 @@ uint16_t ADS834x_GetData(sensor_t * sensor, uint8_t channel)
 void ADS834x_StartDMA(sensor_t * sensor, uint8_t channel)
 {
 
-  //
+	sensor->rx_complete = 0;
+	sensor->tx_complete = 1;
+	sensor->curr_channel = channel;
+
+	if((sensor->type == ADS8341) && (channel >= 4))
+	{
+
+		channel = channel - 4;
+
+	}
+
+	// CS low
+	pin_config[sensor->source].port->ODR &= ~pin_config[sensor->source].pin;
+
+	tmp_buf[0] = 0x87 | ((channel & 0x07) << 4);
+	SPI_FullDuplex_TransmitReceive(&tmp_buf[0],&sensor->data[3*channel], 3, ADS834x_SPI_MODE);
 
 }
 
 void ADS834x_StopDMA(sensor_t * sensor)
 {
 
-  //
+	DMA_Cmd(DMA1_Channel2, DISABLE);
+  
+	// CS high
+	pin_config[sensor->source].port->ODR |= pin_config[sensor->source].pin;
+	sensor->rx_complete = 1;
+	sensor->tx_complete = 1;
 
 }
